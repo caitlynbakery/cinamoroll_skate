f57bfd792288e609c7010e7ce5cce5a51b2c3b1d
Caitlyn <craigoda+caitlyn@gmail.com>
2022-04-03 10:56:27 -0700

    added jump

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,15 @@ void main() {
 
 class MyGame extends FlameGame with TapDetector {
   late SpriteComponent cinamoroll;
+  late SpriteComponent rock;
   Vector2 velocity = Vector2(0, 100);
   double gravity = 5.0;
   String horizontalDirection = "stop";
+  double pushVelocity = 200.0;
+  double groundFriction = 50.0;
+  double jumpVelocity = -100.0;
+
+  bool isJumping = false;
 
   @override
   Future<void>? onLoad() async {
@@ -23,6 +29,12 @@ class MyGame extends FlameGame with TapDetector {
       ..size = Vector2(360 * .3, 450 * .3)
       ..position = Vector2(200, 0);
     add(cinamoroll);
+
+    rock = SpriteComponent()
+      ..sprite = await loadSprite('rock.png')
+      ..size = Vector2(40, 40)
+      ..position = Vector2(600, size[1] - 40);
+    add(rock);
   }
 
   @override
@@ -30,28 +42,39 @@ class MyGame extends FlameGame with TapDetector {
     // TODO: implement update
     super.update(dt);
     if (cinamoroll.position.y < size[1] - cinamoroll.height) {
-      velocity.y = velocity.y + gravity;
+        velocity.y = velocity.y + gravity;
+      
     } else {
-      velocity.y = 0;
+      if (isJumping && jumpVelocity < 0) {
+        print('he is jumping');
+        velocity.y = velocity.y + jumpVelocity;
+        jumpVelocity += gravity;
+      } else {
+        velocity.y = 0;
+        isJumping = false;
+      }
     }
     cinamoroll.position.add(velocity * dt);
 
-  if(cinamoroll.position.x < size[0] - cinamoroll.width){
-    if (horizontalDirection == "right") {
-      velocity.x = velocity.x - dt * 10;
+    if (cinamoroll.position.x < size[0] - cinamoroll.width) {
+      if (horizontalDirection == "right") {
+        if (velocity.x > 0) {
+          velocity.x = velocity.x - dt * groundFriction;
+        }
+      }
+    } else {
+      velocity.x = 0;
     }
-  } else {
-    velocity.x = 0;
-  }
-    
-     if(cinamoroll.position.x > 0){
- if (horizontalDirection == "left") {
-      velocity.x = velocity.x - dt * 10;
+
+    if (cinamoroll.position.x > 0) {
+      if (horizontalDirection == "left") {
+        if (velocity.x < 0) {
+          velocity.x = velocity.x + dt * groundFriction;
+        }
+      }
+    } else {
+      velocity.x = 0;
     }
-  } else{
-    velocity.x = 0;
-  }
-   
   }
 
   @override
@@ -61,12 +84,15 @@ class MyGame extends FlameGame with TapDetector {
     if (info.eventPosition.game.x < 200) {
       print("tapped left");
       horizontalDirection = "left";
-      velocity.x = -100;
+      velocity.x = -1 * pushVelocity;
     } else if (info.eventPosition.game.x > size[0] - 200) {
       horizontalDirection = "right";
-      velocity.x = 100;
+      velocity.x = pushVelocity;
       print("tapped right");
+    } else if (info.eventPosition.game.y < 200) {
+      isJumping = true;
+      jumpVelocity = -100;
+      print("go up!");
     }
-    print(info.eventPosition.game.x);
   }
 }
